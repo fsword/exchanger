5f020e9c652774d4381560c8a280cf8b4c609f46
fsword <li.jianye@gmail.com>
2010-04-02 11:48:43 +0800

    function done: inner_network exchange_ad

--- pv.erl
+++ pv.erl
@@ -1,35 +1,43 @@
 -module(pv).
--export([serv/0,wait/0]).
+-export([start/0,inner_network/2,exchange_ad/3]).
 
 start() -> spawn(fun() -> serv() end).
 
 serv() ->
     receive
-        { RequestPid, RequestId, Catalog } ->
-            spawn(fun() -> pv_process(RequestPid,RequestId,Catalog) end),
-            serv().
+        { Pid, RequestId, Catalog } ->
+            spawn(fun() -> pv_process(Pid,RequestId,Catalog) end),
+        serv()
     end.
-           
-           
-pv_process(RequestPid, RequestId,Catalog) -> 
-    ResponseAd = inner_network(RequestId,Catalog),
-    Ad = exchange_ad(ResponseAd, RequestId,Catalog),
-    RequestPid ! Ad
 
+pv_process(none, RequestId,Catalog) -> 
+    {_, Msg} = exchange_ad(
+        inner_network(RequestId,Catalog),
+        RequestId,
+        Catalog
+    ),
+    io:format("request: ~p~n", [Msg] );
+pv_process(Pid, RequestId,Catalog) -> 
+    Pid ! exchange_ad(
+        inner_network(RequestId,Catalog),
+        RequestId,
+        Catalog
+    ).
 
 inner_network(RequestId,"S") ->
-    {ad,"Sport"};
+    {ad,string:concat("Sport:",RequestId)};
 inner_network(RequestId,"R") ->
-    {ad,"Reading"};
-inner_network(Request,_) -> null.
+    {ad,string:concat("Reading:",RequestId)};
+inner_network(_,_) -> null.
     
 exchange_ad(null,RequestId,"S") ->
-    {ad,"network Sport"};
+    {ad,string:concat("network Sport:",RequestId)};
 exchange_ad(null,RequestId,"R") ->
-    {ad,"network Reading"};
-exchange_ad(null,RequestId,"R") ->
-    {ad,"network others"}.
-    
+    {ad,string:concat("network Reading:",RequestId)};
+exchange_ad(null,RequestId,_) ->
+    {ad,string:concat("network others:",RequestId)};
+exchange_ad(Ad,_,_) -> Ad.
+
 
 %% start a server to receive pv
 %% while true 
