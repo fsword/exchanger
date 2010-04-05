7f3205af76fb3b084a9781388c1ffbba69c4b51d
fsword <li.jianye@gmail.com>
2010-04-05 16:59:05 +0800

    重构，使用不同模块支持交换引擎，可以动态切换模块

--- engine.erl
+++ engine.erl
@@ -1,32 +1,22 @@
--module(pv).
--export([start/0,each_pv/3]).
+-module(engine).
+-export([start/0,start/1,each_pv/3]).
 
-start() -> spawn(fun() -> serv() end).
+start() -> spawn(fun() -> serv(engine) end).
 
-serv() ->
+start(Name) -> register(Name,spawn(fun() -> serv(engine) end)).
+
+serv(Mod) ->
     receive
+        { update, NewMod} ->
+            io:format("module updated: ~p~n", [NewMod] ),
+            serv(NewMod);
         { Pid, RequestId, Catalog } ->
-            spawn(pv,each_pv,[Pid,RequestId,Catalog]),
-        serv()
+            spawn(Mod,each_pv,[Pid,RequestId,Catalog]),
+            serv(Mod)
     end.
 
-each_pv(none, RequestId,Catalog) -> 
-    {_, Msg} = exchange:disp(
-        inner:campaign(RequestId,Catalog),
-        RequestId,
-        Catalog
-    ),
-    io:format("request: ~p~n", [Msg] );
 each_pv(Pid, RequestId,Catalog) -> 
-    Pid ! exchange:disp(
-        inner:campaign(RequestId,Catalog),
-        RequestId,
-        Catalog
-    ).
-
-    
-
-
+    io:format("request: Pid = ~p, RequestId = ~p, Catalog = ~p.~n", [Pid,RequestId,Catalog] ).
 
 %% start a server to receive pv
 %% while true 
--- exchange.erl
+++ exchange.erl
@@ -1,5 +1,25 @@
 -module(exchange).
--export([disp/3]).
+-export([each_pv/3]).
+
+each_pv(none, RequestId,Catalog) -> 
+    {_, Msg} = disp(
+        campaign(RequestId,Catalog),
+        RequestId,
+        Catalog
+    ),
+    io:format("request: ~p~n", [Msg] );
+each_pv(Pid, RequestId,Catalog) -> 
+    Pid ! disp(
+        campaign(RequestId,Catalog),
+        RequestId,
+        Catalog
+    ).
+
+campaign(RequestId,"S") ->
+    {ad,string:concat("Sport:",RequestId)};
+campaign(RequestId,"R") ->
+    {ad,string:concat("Reading:",RequestId)};
+campaign(_,_) -> null.
 
 disp(null,RequestId,"S") ->
     {ad,string:concat("network Sport:",RequestId)};
@@ -8,3 +28,5 @@ disp(null,RequestId,"R") ->
 disp(null,RequestId,_) ->
     {ad,string:concat("network others:",RequestId)};
 disp(Ad,_,_) -> Ad.
+
+
