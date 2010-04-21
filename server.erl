c1b1adbcbeedb6d003e743045fbb275e296d4963
fsword <li.jianye@gmail.com>
2010-04-21 22:46:09 +0800

    小修改一下

--- server.erl
+++ server.erl
@@ -5,7 +5,7 @@
 
 % 启动一个模块，注册服务的名称为模块名称 %
 start(Mod) -> spawn(server,init,[Mod]).
-start(Mod,Req) -> spawn(server,init,[Mod,Req]).
+start(Mod,Env) -> spawn(server,init,[Mod,Env]).
 
 %请求响应式接口，具体的实现实际上依靠Mod模块的 handle_request 方法%
 request(Mod, Request) ->
@@ -13,7 +13,7 @@ request(Mod, Request) ->
     ok.
 %消息发送接口，请求进程不处理后续事件%
 send(Mod, Event) ->
-    Mod ! { trans, Event },
+    Mod ! { send, Event },
     ok.
 
 % 初始化服务，注册名称，启动init方法 %
@@ -23,9 +23,9 @@ init(Mod) ->
         loop(Mod,State).
 
 % 初始化服务时接受一个参数 %
-init(Mod, Req) -> 
+init(Mod, Env) -> 
         register(Mod,self()),
-        State = Mod:init(Req),
+        State = Mod:init(Env),
         loop(Mod,State).
 
 % 实际执行体 %
@@ -35,8 +35,8 @@ loop(Mod, State) ->
             {Res,State2} = Mod:handle_request(Request,State),
             From ! {Mod, Res},
             loop(Mod,State2);
-        { event, Event } ->
-            State2 = Mod:handle_event(State, Event),
+        { send, Event } ->
+            State2 = Mod:handle_event(Event,State),
             loop(Mod,State2)
     end.
 
